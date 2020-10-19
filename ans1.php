c8600b75e9565e69bc04c110c92788ee5d41da84
asaliu <kbman437@gmail.com>
2020-10-19 21:41:07 +0800

    完成第一題

---
+++ ans1.php
@@ -0,0 +1,61 @@
+<?php
+
+interface limit{
+    public function speed();
+}
+
+class Car{
+    protected $tire="";
+    protected $volume="";
+    protected $color="";
+
+    public function __construct($tire,$volume,$color){
+        $this->tire=$tire;
+        $this->volume=$volume;
+        $this->color=$color;
+    }
+
+    public function tire(){
+        return $this->tire;
+    }
+    public function volume(){
+        return $this->volume;
+    }
+    public function color(){
+        return $this->color;
+    }
+}
+
+echo "car";
+echo "<br>";
+$car=new Car(4,'big','red');
+echo "輪胎:" . $car->tire();
+echo "<br>";
+echo "體積:" . $car->volume();
+echo "<br>";
+echo "顏色:" . $car->color();
+echo "<br>";
+
+echo "<hr>";
+
+class Scooter extends Car implements limit{
+    public function speed(){
+        echo "run slowly";
+    }
+}
+
+echo "scooter";
+echo "<br>";
+
+$scooter=new Scooter(2,'small','gray');
+echo "輪胎:" . $scooter->tire();
+echo "<br>";
+echo "體積:" . $scooter->volume();
+echo "<br>";
+echo "顏色:" . $scooter->color();
+echo "<br>";
+echo  $scooter->speed();
+echo "<br>";
+
+
+?>
